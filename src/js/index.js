c41c89b43d467b6b9cca51712cc253d489568924
yechuanjie <1102684711@qq.com>
2018-05-23 19:11:06 +0800

    样式修改

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,38 @@
 // import 'babel-polyfill';
-import { wnlShare, wxShare, toast } from '@wnl/ui';
+// import { wnlShare, wxShare, toast } from '@wnl/ui';
 import { util } from '@wnl/util';
 import '../scss/index.scss';
 import '../static/flexable';
 
-let idnexShareInfo = {
-  title: `挖金小游戏`,
-  text: '挖金小游戏',
-  image: 'https://qiniu.image.cq-wnl.com/content/2018051865471c3699f4494797becd7060b895df.png',
-  url: `${window.location.href}${window.location.href.indexOf('?') > -1 ? '&' : '?'}share=1`
-};
+// let idnexShareInfo = {
+//   title: `挖金小游戏`,
+//   text: '挖金小游戏',
+//   image: 'https://qiniu.image.cq-wnl.com/content/2018051865471c3699f4494797becd7060b895df.png',
+//   url: `${window.location.href}${window.location.href.indexOf('?') > -1 ? '&' : '?'}share=1`
+// };
 
-if (util.isWnl) {
-  wnlShare.setShareData({
-    title: idnexShareInfo.title,
-    text: idnexShareInfo.text,
-    image: idnexShareInfo.image,
-    url: idnexShareInfo.url
-  });
-}
-if (util.isWeixin) {
-  wxShare({
-    title: idnexShareInfo.title,
-    text: idnexShareInfo.text,
-    imgUrl: idnexShareInfo.image,
-    url: idnexShareInfo.url,
-    callback: () => {
-      // 分享成功回调
-    }
-  });
-}
+// if (util.isWnl) {
+//   wnlShare.setShareData({
+//     title: idnexShareInfo.title,
+//     text: idnexShareInfo.text,
+//     image: idnexShareInfo.image,
+//     url: idnexShareInfo.url
+//   });
+// }
+// if (util.isWeixin) {
+//   wxShare({
+//     title: idnexShareInfo.title,
+//     text: idnexShareInfo.text,
+//     imgUrl: idnexShareInfo.image,
+//     url: idnexShareInfo.url,
+//     callback: () => {
+//       // 分享成功回调
+//     }
+//   });
+// }
 $(() => {
-  new toast().show('toast~'); //eslint-disable-line
+  // new toast().show('toast~'); //eslint-disable-line
+  console.log(util.isIphoneX);
   let hook = $('#hook');
   let line = $('#line');
   console.log(hook.offset());
@@ -62,14 +63,16 @@ $(() => {
   // 动画
   const screenWidth = $(window).width();
   const screenHeight = $(window).height();
-  const landHeight = screenHeight - 235;
-  const MIN_ROPE_LENGTH = 10;
-  const MAX_ROPE_LENGTH = Math.sqrt(Math.pow(screenWidth / 2, 2) + Math.pow(landHeight, 2)); // eslint-disable-line
-  const MAX_ROTATE = 60;
-  const MIN_ROTATE = -60;
-  const hookHeight = $('#hook').height() + MIN_ROPE_LENGTH;
-  // const hookHeight = 0;
+  const hookHeight = $('#hook').height();
 
+
+  const MAX_ROTATE = 70;
+  const MIN_ROTATE = -70;
+  const MIN_ROPE_LENGTH = 7;
+  // 全面屏时，土地比例分配更多
+  const percent = screenHeight >= 800 ? 0.7389 : 0.6866;
+  const landHeight = (screenHeight * percent) - hookHeight - MIN_ROPE_LENGTH;
+  const MAX_ROPE_LENGTH = Math.sqrt(Math.pow(screenWidth / 2, 2) + Math.pow(landHeight, 2)); // eslint-disable-line
 
   let xx = 0;
   let yy = 0;
@@ -82,8 +85,9 @@ $(() => {
       this.actionInterval = null;
       this.manImgList = [];
       this.count = 0;
+      this.oreList = [];
       this.initHookAnimation();
-      this.initOreLocation();
+      this.getOreLocation();
       $('.main_container').on('click', () => {
         console.log('rr', rr, 'hh', hh);
         // 当绳子没有伸缩时，点击时才执行动画，避免重复执行
@@ -93,18 +97,43 @@ $(() => {
         }
       });
     }
-    initOreLocation() {
-      $('.bottom_wrapper img').each(function() {
-        let x = 50;
-        let y = 50;
-        $(this).css({
-          transform: `translate3d(${x}px, ${y}px, 0)`
-        });
+    getOreLocation() {
+      let that = this;
+      $('.ore_list img').each(function() {
+        // let x = 0;
+        // let y = 0;
+        // $(this).css({
+        //   transform: `translate3d(${x}px, ${y}px, 0)`
+        // });
+        let width = $(this).width();
+        let height = $(this).height();
+        let x = $(this).offset().left;
+        let y = $(this).offset().top;
+        // 矿石最右侧的left值
+        let xr = x + width;
+        // 矿石最底部的top值
+        let yb = y + height;
+        let thisInfo = {
+          x,
+          y,
+          xr,
+          yb
+        };
+        that.oreList.push(thisInfo);
       });
+      console.log(this.oreList);
     }
+    /**
+     * 初始动画
+     * @memberof GAME
+     */
     initHookAnimation() {
       this.animateInterval = setInterval(this.hookAnimation, 30);
     }
+    /**
+     * 钩子摆动动画
+     * @memberof GAME
+     */
     hookAnimation() {
       if (!FINISH) {
         if (rr > MIN_ROTATE) {
@@ -125,18 +154,30 @@ $(() => {
         height: `${hh}px`
       });
     }
+    /**
+     * 绳子动画控制
+     * @memberof GAME
+     */
     lineAnimation() {
       let width = screenWidth / 2;
-      let rad = Math.sin(Math.abs(rr) * (Math.PI / 180));
-      let ropeHeight = (width / rad);
-      console.log('ropeHeight', ropeHeight);
+      //弧度
+      let rad = Math.abs(rr) * (Math.PI / 180);
+      // 绳子长度，需要减掉钩子长度
+      let ropeHeight = (width / Math.sin(rad)) - hookHeight;
+      console.log('rope', ropeHeight);
+      console.log('MAX_ROPE_LENGTH', MAX_ROPE_LENGTH);
       if (ropeHeight > MAX_ROPE_LENGTH) {
-        //  ropeHeight = landHeight / cos(rr)
-        ropeHeight = MAX_ROPE_LENGTH - hookHeight;
+        ropeHeight = (landHeight / Math.cos(rad)) + 30;
+        console.log('ropeHeight', ropeHeight);
       }
       let extend = setInterval(() => {
         if (hh < ropeHeight) {
           hh += 1;
+          if (this.checkImpact()) {
+            console.log(this.checkImpact());
+            clearInterval(extend);
+            this.recover();
+          }
         } else {
           clearInterval(extend);
           this.recover();
@@ -147,8 +188,39 @@ $(() => {
         });
       }, 10);
     }
+    /**
+     * 碰撞检测
+     * @memberof GAME
+     */
+    checkImpact() {
+      let width = hook.width();
+      let height = hook.height();
+      let x1 = hook.offset().left;
+      let y1 = hook.offset().top;
+      let xr1 = x1 + width;
+      let yb1 = y1 + height;
+      // let hookPosition = {
+      //   x1, y1, xr1, yb1
+      // };
+      let impactNum = null;
+      for (let i = 0; i < this.oreList.length; i += 1) {
+        if (this.oreList[i].yb < y1 || this.oreList[i].x > xr1 || this.oreList[i].y > yb1 || this.oreList[i].xr < x1) {
+          console.log('未碰撞');
+        } else {
+          console.log(`和第${i + 1}个物体碰撞`);
+          impactNum = i + 1;
+        }
+      }
+      return impactNum;
+      // console.log(hookPosition);
+    }
+    /**
+     * 绳子收回动画
+     * @memberof GAME
+     */
     recover() {
       this.manAction();
+      this.handleRecoverAction();
       let interval = setInterval(() => {
         if (hh > MIN_ROPE_LENGTH) {
           hh -= 1;
@@ -164,6 +236,17 @@ $(() => {
         });
       }, 10);
     }
+    /**
+     * 处理绳子收回时有矿石的方法
+     * @memberof GAME
+     */
+    handleRecoverAction() {
+
+    }
+    /**
+     * 绳子收回时，矿工动画
+     * @memberof GAME
+     */
     manAction() {
       require.ensure([], () => {
         /* eslint-disable */
@@ -181,6 +264,10 @@ $(() => {
         this.count += 1;
       }, 40);
     }
+    /**
+     * 格式化数字
+     * @memberof GAME
+     */
     formatNumber(n) {
       n = n.toString();
       return n[1] ? n : '0' + n;
